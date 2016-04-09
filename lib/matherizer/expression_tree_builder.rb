7a679986ec5eea8e9b5878f30fca3829f5fac236
Sean Sellek <me@seansellek.com>
2016-04-09 17:37:36 -0400

    Completed expression tree builder

--- lib/matherizer/expression_tree_builder.rb
+++ lib/matherizer/expression_tree_builder.rb
@@ -1,7 +1,18 @@
 module Matherizer
   module ExpressionTreeBuilder
     def self.call sexp
-      
+      OperationNode.new(sexp[0]) do |n|
+        n.left_operand  = operand_node_for sexp[1]
+        n.right_operand = operand_node_for sexp[2]
+      end
+    end
+
+    def self.operand_node_for operand
+      if operand.is_a?(Array)
+        self.call(operand)
+      else
+        ValueNode.new(operand)
+      end
     end
   end
 end
--- spec/matherizer_spec.rb
+++ spec/matherizer_spec.rb
@@ -16,25 +16,25 @@ describe Matherizer do
 
   describe OperationNode do
     it "performs operation on both operadnds" do
-      node = OperationNode.new(:+, 1, 1)
+      node = OperationNode.new(:+, ValueNode.new(1), ValueNode.new(1))
       expect(node.value).to eq 2 
     end
 
     it "accepts nested operators" do
       node = OperationNode.new(:*)
-      node.right_operand = OperationNode.new(:+, 1, 1)
-      node.left_operand = 2
+      node.right_operand = OperationNode.new(:+, ValueNode.new(1), ValueNode.new(1))
+      node.left_operand = ValueNode.new(2)
       expect(node.value).to eq 4
     end
 
     it "accepts nested operators through blocks" do
       node =  OperationNode.new(:-) do |n|
-        n.right_operand = -6
+        n.right_operand = ValueNode.new(-6)
         n.left_operand = OperationNode.new(:*) do |n|
-          n.right_operand = 4
+          n.right_operand = ValueNode.new(4)
           n.left_operand = OperationNode.new(:/) do |n|
-            n.left_operand = 2
-            n.right_operand = OperationNode.new(:+, 2, 3.33)
+            n.left_operand = ValueNode.new(2)
+            n.right_operand = OperationNode.new(:+, ValueNode.new(2), ValueNode.new(3.33))
           end
         end
       end
@@ -65,6 +65,41 @@ describe Matherizer do
         token_array = ["(", "1", "+", "1", ")", "/", "2"]
         expect(Tokenizer::Converter.call(token_array)).to eq(["(", 1.0, :+, 1.0, ")", :/, 2.0])
       end
+    end
+  end
+
+  describe ExpressionTreeBuilder do
+    it "builds expression tree for simple s expression" do
+      sexp = [:+, 1, 1]
+      expression = OperationNode.new(:+, ValueNode.new(1), ValueNode.new(1))
+      expect(ExpressionTreeBuilder.call(sexp).value).to eq(expression.value)
+    end
+
+    it "builds expression tree for nested s expression" do
+      sexp = [:+, 1, [:*, 1, 2]]
+      expression = OperationNode.new(:+) do |n|
+        n.left_operand = ValueNode.new(1)
+        n.right_operand = OperationNode.new(:*) do |n|
+          n.left_operand = ValueNode.new(1)
+          n.right_operand = ValueNode.new(2)
+        end
+      end
+      expect(ExpressionTreeBuilder.call(sexp).value).to eq(expression.value)
+    end
+
+    it "builds expression tree for complex s expression" do
+      sexp = [:-, [:*, [:/, 2, [:+, 2, 3.33]], 4], -6]
+      expression = OperationNode.new(:-) do |n|
+        n.right_operand = ValueNode.new(-6)
+        n.left_operand = OperationNode.new(:*) do |n|
+          n.right_operand = ValueNode.new(4)
+          n.left_operand = OperationNode.new(:/) do |n|
+            n.left_operand = ValueNode.new(2)
+            n.right_operand = OperationNode.new(:+, ValueNode.new(2), ValueNode.new(3.33))
+          end
+        end
+      end
+      expect(ExpressionTreeBuilder.call(sexp).value).to eq(expression.value)
     end
   end
 end
