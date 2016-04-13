148f4cc59f830d3df1d173b695a87a9c8c4ea92a
Sean Sellek <me@seansellek.com>
2016-04-13 18:50:59 -0400

    Removed stringscanner

--- lib/matherizer/tokenizer/splitter.rb
+++ lib/matherizer/tokenizer/splitter.rb
@@ -1,9 +1,7 @@
-require 'strscan'
 module Matherizer
   module Tokenizer
     module Splitter
       def self.call(expression)
-        s = StringScanner.new(expression)
         expression.gsub(/([^\s\d\.\*]|\*\*)/, ' \1 ').split(' ')
       end
     end
